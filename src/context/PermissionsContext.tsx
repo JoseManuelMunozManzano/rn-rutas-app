fdd41a22c716f3b971d9cde253b0b6deef1beb94
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2021-05-23 09:12:03 +0200

    Uso del PermissionContext

--- src/context/PermissionsContext.tsx
+++ src/context/PermissionsContext.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {createContext, useState} from 'react';
-import {PermissionStatus} from 'react-native-permissions';
+import {Platform} from 'react-native';
+import {PERMISSIONS, PermissionStatus, request} from 'react-native-permissions';
 
 export interface PermissionsState {
   locationStatus: PermissionStatus;
@@ -25,7 +26,19 @@ export const PermissionsProvider = ({
 }) => {
   const [permissions, setPermissions] = useState(permissionInitState);
 
-  const askLocationPermission = () => {};
+  const askLocationPermission = async () => {
+    let permissionStatus: PermissionStatus;
+
+    if (Platform.OS === 'ios') {
+      permissionStatus = await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+    } else {
+      permissionStatus = await request(
+        PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
+      );
+    }
+
+    setPermissions({...permissions, locationStatus: permissionStatus});
+  };
 
   const checkLocationPermission = () => {};
 
